0b1206f9bed86718521ce8e316d51123b536f2da
marcopibbes <marco.bardazzi1@stud.unifi.it>
2026-02-06 10:57:45 +0100

    Added Vite+React

--- full-stack/riecologgames/src/services/GameService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = "http://localhost:8080/api/games";
-
-export const listGames = () => {
-    return axios.get(API_BASE_URL);
-}
--- full-stack/riecologgames/src/services/UserService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = "http://localhost:8080/api/users";
-
-export const listEmployees = () => {
-    return axios.get(API_BASE_URL);
-}
